d21d3e550ec9c4c04dcb5edce24719db2fc3525e
Christopher Carmona <c.carmona0990@gmail.com>
2021-12-21 22:34:59 -0400

    update
    allow cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // server.js
 const server = require('express')();
-const zelda_games = require('./data')
+const zelda_games = require('./data');
+const cors = require('cors');
 const port = process.env.PORT || 5500;
+
+server.use(cors({
+    origin: '*'
+}))
 
 server.get('/',(req,res)=>{
     res.json(zelda_games)
