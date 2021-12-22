01c92048bfb54010232ae7573fb91e06830d86d5
Christopher Carmona <c.carmona0990@gmail.com>
2021-12-21 22:19:32 -0400

    update
    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const server = require('express')();
 const zelda_games = require('./data')
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5500;
 
 server.get('/',(req,res)=>{
     res.json(zelda_games)
